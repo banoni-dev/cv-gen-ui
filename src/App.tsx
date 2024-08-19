e7f7688df6304f9ee3645f3457350e843f618bca
Ahmed Barhoumi <banoni.bro@gmail.com>
2024-08-19 21:38:55 +0100

    feat(App.tsx): replace fetch with axios for PDF generation and improve error handling refactor(App.tsx): enhance PDF download logic and structure the JSX for better readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
+import axios from "axios";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
   const [value, setValue] = useState("");
+
   const handleClick = async () => {
     try {
-      fetch("http://localhost:3000/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ jobDescription: value }),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-        });
+      const response = await axios.post("http://localhost:3000/generate", {
+        jobDescription: value,
+      });
+      const { pdfPath } = response.data;
+
+      if (pdfPath) {
+        console.log("PDF generated successfully", pdfPath);
+        const downloadUrl = `http://localhost:3000/download-pdf/${pdfPath}`;
+        const a = document.createElement("a");
+        a.href = downloadUrl;
+        a.download = pdfPath;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      } else {
+        console.error("Failed to generate PDF");
+      }
     } catch (error) {
-      console.error(error);
+      console.error("Error generating PDF:", error);
     }
   };
+
   return (
     <>
-      <textarea
-        cols={80}
-        rows={30}
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      ></textarea>
-      <button onClick={handleClick}>Submit</button>
+      <div>
+        <textarea
+          cols={80}
+          rows={30}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        ></textarea>
+        <button onClick={handleClick}>Submit</button>
+      </div>
     </>
   );
 }
